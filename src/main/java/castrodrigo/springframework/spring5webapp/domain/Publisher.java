5f21368df5f7e7eff5de50fd1fc68e61b16f4e00
castrodrigo <castrodrigo@yahoo.com.br>
2020-04-09 23:20:54 +0200

    Create basic models

---
+++ src/main/java/castrodrigo/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,110 @@
+package castrodrigo.springframework.spring5webapp.domain;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Publisher {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String addressLine1;
+  private String city;
+  private String state;
+  private String zipcode;
+
+  @OneToMany
+  @JoinColumn(name = "publisher_id")
+  private Set<Book> books = new HashSet<>();
+
+  public Publisher() {
+  }
+
+  public Publisher(String addressLine1, String city, String state, String zipcode) {
+    this.addressLine1 = addressLine1;
+    this.city = city;
+    this.state = state;
+    this.zipcode = zipcode;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getAddressLine1() {
+    return addressLine1;
+  }
+
+  public void setAddressLine1(String addressLine1) {
+    this.addressLine1 = addressLine1;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public String getZipcode() {
+    return zipcode;
+  }
+
+  public void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
+
+  public Set<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(Set<Book> books) {
+    this.books = books;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this)
+      return true;
+    if (!(o instanceof Publisher)) {
+      return false;
+    }
+    Publisher publisher = (Publisher) o;
+    return Objects.equals(id, publisher.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
+
+  @Override
+  public String toString() {
+    return "{" + " id='" + getId() + "'" + ", addressLine1='" + getAddressLine1() + "'" + ", city='" + getCity() + "'"
+        + ", state='" + getState() + "'" + ", zipcode='" + getZipcode() + "'" + ", books='" + getBooks() + "'" + "}";
+  }
+
+}
---
+++ src/main/java/castrodrigo/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package castrodrigo.springframework.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import castrodrigo.springframework.spring5webapp.domain.Author;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
